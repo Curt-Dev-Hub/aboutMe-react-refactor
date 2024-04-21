1857e8de126693fc6586aef36a2e138b08908f41
Curtis Dev K <curt_king@coolsite.net>
2024-04-21 13:51:59 +0100

    update vite.config base property

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/aboutMe-react-refactor/",
-  base: "./",
+  base: "/aboutMe-react-refactor/",
   plugins: [react()],
   // resolve: {
   //   alias: {
